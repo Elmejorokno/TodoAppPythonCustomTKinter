c1e82c560456504ec3940c8b81677af2db3417e9
ferdy <ferdyiglesias12@gmail.com>
2024-11-28 19:36:45 -0600

    second commit

--- todo/views/home_window.py
+++ todo/views/home_window.py
@@ -6,24 +6,30 @@ from todo.views.register_window import register_window
 
 def home_window():
     def handle_register():
-        app.destroy()
-        register_window()
+        app.withdraw()
+        register_window(app)
 
     def handle_login():
-        app.destroy()
-        login_window()
+        app.withdraw()
+        login_window(app)
 
     def handle_exit():
         app.destroy()
 
     app = tk.CTk()
     app.geometry('300x300')
+    app.title("DoIt - Inicio")
 
-    button_register = tk.CTkButton(app, text='Registrarse', corner_radius=20, command=handle_register)
-    button_register.place(relx=0.5, rely=0.2, anchor='center')
+    label_application = tk.CTkLabel(app, text="DoIt", font=("Arial", 50, "bold"), text_color="#942492")
+    label_application.place(relx=0.5, rely=0.15, anchor="center")
 
-    button_login = tk.CTkButton(app, text='Iniciar Sesión', corner_radius=20, command=handle_login)
-    button_login.place(relx=0.5, rely=0.4, anchor='center')
+    button_register = tk.CTkButton(app, text='Registrarse', corner_radius=20, fg_color="transparent", hover_color="#942492",
+                                border_color="#942492", border_width=2, command=handle_register)
+    button_register.place(relx=0.5, rely=0.35, anchor='center')
+
+    button_login = tk.CTkButton(app, text='Iniciar Sesión', corner_radius=20, fg_color="transparent", hover_color="#942492",
+                                border_color="#942492", border_width=2, command=handle_login)
+    button_login.place(relx=0.5, rely=0.47, anchor='center')
 
 
     # Ejecutar la aplicación
--- todo/views/register_window.py
+++ todo/views/register_window.py
@@ -1,9 +1,52 @@
-import tkinter as tk
+import customtkinter as tk
 
-def register_window():
-    root = tk.Tk()
-    root.title("Registrarse")
-    root.geometry("300x300")
+def register_window(root):
+    checked_password = False
 
-    root.mainloop()
+    def handle_register():
+        pass
+
+    def handle_comeback():
+        app.destroy()
+        root.deiconify()
+
+    def handle_showpassword():
+        if checkbox_password.get():
+            entry_password.configure(show="")
+        else:
+            entry_password.configure(show="*")
+
+    def handle_close():
+        app.destroy()
+        root.destroy()
+
+    app = tk.CTkToplevel(root)
+    app.title("DoIt - Registrarse")
+    app.geometry("400x400")
+
+    app.protocol("WM_DELETE_WINDOW", handle_close)
+
+    label_username = tk.CTkLabel(app, text="Username:", font=("Arial", 20, "bold"), text_color="#fff")
+    label_username.place(relx=0.08, rely=0.1)
+    entry_username = tk.CTkEntry(app, text_color="#fff")
+    entry_username.place(relx=0.44, rely=0.1)
+
+    label_password = tk.CTkLabel(app, text="Contraseña:", font=("Arial", 20, "bold"), text_color="#fff")
+    label_password.place(relx=0.08, rely=0.2)
+    entry_password = tk.CTkEntry(app, text_color="#fff", show="*")
+    entry_password.place(relx=0.44, rely=0.2)
+
+    checkbox_password = tk.CTkCheckBox(app, text="Mostrar contraseña", fg_color="#942492",
+                                       checkbox_width=20, checkbox_height=20, command=handle_showpassword)
+    checkbox_password.place(relx=0.62, rely=0.32, anchor='center')
+
+    button_login = tk.CTkButton(app, text='Registrarse', corner_radius=20, fg_color="transparent",
+                                hover_color="#942492",
+                                border_color="#942492", border_width=2, command=handle_register)
+    button_login.place(relx=0.5, rely=0.45, anchor='center')
+
+    button_comeback = tk.CTkButton(app, text='Regresar', corner_radius=20, fg_color="transparent",
+                                hover_color="#942492",
+                                border_color="#942492", border_width=2, command=handle_comeback)
+    button_comeback.place(relx=0.5, rely=0.55, anchor='center')
 
